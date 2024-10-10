9fbff86998ccc44ee3d7fc9d850ae2aade2e2b0f
seanrandevs <93683781+seanrandevs@users.noreply.github.com>
2024-10-10 16:44:32 -0500

    Added some of the Navbar
    Added a few Navbar sections

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,10 +1,14 @@
+import NavBar from "./Nav_bar";
 
 const Homepage = () => {
   return (
+    <>
+    <NavBar />
     <div>
       <h1>Welcome to the Event Management System</h1>
       <p>Explore upcoming events, manage RSVPs, and more.</p>
     </div>
+    </>
   );
 };
 
