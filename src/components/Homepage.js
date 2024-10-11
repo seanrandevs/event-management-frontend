82730e2f0262cf699db4b59d38e4498402dac92a
seanrandevs <93683781+seanrandevs@users.noreply.github.com>
2024-10-11 15:24:49 -0500

    Creating the homepage
    Added icons to navbar and creating the homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,13 +1,27 @@
 import NavBar from "./Nav_bar";
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
 
 const Homepage = () => {
   return (
     <>
     <NavBar />
-    <div>
-      <h1>Welcome to the Event Management System</h1>
-      <p>Explore upcoming events, manage RSVPs, and more.</p>
-    </div>
+    <Container>
+      <div className="homepage">
+      <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+        <h1>Welcome to EventSnap</h1>
+        <p>Explore upcoming events, manage RSVPs, and more.</p>
+      </div>
+    </Container>
     </>
   );
 };
--- src/components/Nav_bar.js
+++ src/components/Nav_bar.js
@@ -1,17 +1,20 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { VscAccount, VscTerminalDebian  } from "react-icons/vsc";
 
 const Nav_bar = () => {
   return (
     <div>
       <Navbar bg="light" data-bs-theme="light">
         <Container>
+        <VscTerminalDebian className="logo" />
           <Navbar.Brand href="#home">EventSnap</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#browseevents">Browse Events</Nav.Link>
           </Nav>
         </Container>
+        <VscAccount className="account-logo" />
       </Navbar>
     </div>
   )
