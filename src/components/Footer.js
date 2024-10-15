470c9dd33f2d247cfbcc92a292b89ac73015f692
seanrandevs <93683781+seanrandevs@users.noreply.github.com>
2024-10-15 16:08:18 -0500

    Added carousel and footer
    Added a carousel and images to the homepage and added some links to the footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,28 @@
 const Footer = () => {
   return (
     <div className="footer">
-        <h1>Footer</h1>
+      <div className="left"></div>
+      <div className="right">
+        <div className="helpful-links">
+          <h5>Helpful Links</h5>
+          <ul>
+            <li>Help/FAQ</li>
+            <li>My Account</li>
+            <li>Contact Us</li>
+          </ul>
+        </div>
+        <div className="about-us">
+          <h5>About Us</h5>
+          <ul>
+            <li>Who We Are</li>
+            <li>Privacy Policy</li>
+            <li>Ad Choices</li>
+          </ul>
+        </div>
+      </div>
+      <hr style={{ border: '1px solid #000000' }} />
     </div>
+
   )
 }
-
 export default Footer
--- src/components/HomeCarousel.js
+++ src/components/HomeCarousel.js
@@ -1,10 +1,47 @@
-
+import Carousel from 'react-bootstrap/Carousel';
+import Carousel3 from '../assets/images/carousel3.jpg'
+import Carousel2 from '../assets/images/carousel2.jpg'
+import Carousel1 from '../assets/images/carousel1.jpg'
 
 const HomeCarousel = () => {
   return (
-    <div>
-
-    </div>
+    <Carousel>
+      <Carousel.Item>
+      <img
+          className="d-block w-100"
+          src={Carousel3}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <img
+          className="d-block w-100"
+          src={Carousel2}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <img
+          className="d-block w-100"
+          src={Carousel1}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
   )
 }
 
